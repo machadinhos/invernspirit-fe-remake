f32aca7900ea4a306e53a0d103a1543a8eca25cb
machadinhos <pbmachado2002@gmail.com>
2024-10-20 23:59:41 +0100

    add correct images urls and use URL object to validate url

--- src/lib/utils/links.ts
+++ src/lib/utils/links.ts
@@ -1,15 +1,29 @@
+const baseDomain = 'invernspirit.com';
+const protocol = 'https://';
+
+function generateInternalURL(pathName: string, subDomain?: string) {
+  const fullDomain = subDomain
+    ? `${subDomain}${subDomain.endsWith('.') ? '' : '.'}${baseDomain}`
+    : baseDomain;
+
+  const fullPath = pathName.startsWith('/') ? pathName : `/${pathName}`;
+
+  return new URL(`${protocol}${fullDomain}${fullPath}`);
+}
+
 export const imageLinks = {
   index: {
     'bg-images': [
-      'https://images.lamaisonregina.com/gallery/garden/2.webp',
-      'https://images.lamaisonregina.com/gallery/garden/3.webp',
-      'https://images.lamaisonregina.com/gallery/garden/1.webp'
+      generateInternalURL('/products/ceramics-product-1-1.jpeg', 'images.'),
+      generateInternalURL('/products/ceramics-product-2-1.jpeg', 'images.'),
+      generateInternalURL('/products/ceramics-product-3-1.jpeg', 'images.'),
+      generateInternalURL('/products/ceramics-product-4-1.jpeg', 'images.')
     ]
   }
 };
 
 export const socialMediaLinks = {
-  instagram: 'https://www.lamaisonregina.com/pt',
-  pinterestP: 'https://www.lamaisonregina.com/pt',
-  youtube: 'https://www.lamaisonregina.com/pt'
+  instagram: new URL('https://www.lamaisonregina.com/pt'),
+  pinterestP: new URL('https://www.lamaisonregina.com/pt'),
+  youtube: new URL('https://www.lamaisonregina.com/pt')
 };
