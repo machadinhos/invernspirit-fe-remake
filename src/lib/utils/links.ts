bacf41aa9ed8369ff77ae1804390747bc557174d
machadinhos <pbmachado2002@gmail.com>
2024-10-20 17:51:57 +0100

    add home page bg image

---
+++ src/lib/utils/links.ts
@@ -0,0 +1,9 @@
+export const imageLinks = {
+  index: {
+    'bg-images': [
+      'https://images.lamaisonregina.com/gallery/garden/2.webp',
+      'https://images.lamaisonregina.com/gallery/garden/3.webp',
+      'https://images.lamaisonregina.com/gallery/garden/1.webp'
+    ]
+  }
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { imageLinks } from '$lib/utils/links';
+
+  let currentImageIndex = 0;
+  let nextImageIndex = 1;
+
+  function rotateImages() {
+    nextImageIndex = (currentImageIndex + 1) % imageLinks.index['bg-images'].length;
+    setTimeout(() => {
+      currentImageIndex = nextImageIndex;
+    }, 500);
+  }
+
+  setInterval(rotateImages, 5000);
+</script>
+
+<div class="absolute inset-0 h-full w-full overflow-hidden bg-gray-600 grayscale filter">
+  {#each imageLinks.index['bg-images'] as image, index}
+    <div
+      class="fixed h-full w-full bg-cover bg-center bg-no-repeat transition-opacity duration-500"
+      class:opacity-100={index === currentImageIndex}
+      class:opacity-0={index !== currentImageIndex}
+      style="background-image: url({image});"
+    />
+  {/each}
+</div>
