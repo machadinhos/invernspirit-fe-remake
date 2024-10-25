8268c4af7e9a516889390ceea842dc895603e282
machadinhos <pbmachado2002@gmail.com>
2024-10-25 19:15:45 +0100

    add HeaderRightSection

--- src/lib/components/layout/header/left-section/HeaderLeftSection.svelte
+++ src/lib/components/layout/header/left-section/HeaderLeftSection.svelte
@@ -3,7 +3,7 @@
   import NewsletterSub from "$lib/components/layout/header/left-section/NewsletterSub.svelte";
 </script>
 
-<div class="mx-10 flex w-fit gap-4">
+<div class="flex w-fit gap-4">
   <NewsletterSub />
   <div class="h-9 w-0.5 bg-white"></div>
   <MediaButtonsSection />
--- src/lib/components/layout/header/right-section/HeaderRightSection.svelte
+++ src/lib/components/layout/header/right-section/HeaderRightSection.svelte
@@ -1,1 +1,18 @@
-<div></div>
+<script>
+  import HeaderIcon from "$lib/components/layout/header/HeaderIcon.svelte";
+  import { BiSolidCart } from "svelte-icons-pack/bi";
+  import { FaSolidUser } from "svelte-icons-pack/fa";
+</script>
+
+<div class="flex items-center justify-end gap-4">
+  <select class="h-fit bg-background text-white">
+    <option>pt</option>
+    <option>es</option>
+  </select>
+  <a href="/" class="h-fit w-fit">
+    <HeaderIcon src={FaSolidUser} />
+  </a>
+  <a href="/" class="h-fit w-fit">
+    <HeaderIcon src={BiSolidCart} />
+  </a>
+</div>
