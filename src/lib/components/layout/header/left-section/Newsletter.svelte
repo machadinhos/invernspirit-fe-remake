15c4325493089ed43ed5c32fd0833761c7293cb1
machadinhos <pbmachado2002@gmail.com>
2024-10-27 21:48:01 +0000

    substitute smui TextField to our own TextInput

--- src/lib/components/layout/header/left-section/Newsletter.svelte
+++ src/lib/components/layout/header/left-section/Newsletter.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { Icon } from "svelte-icons-pack";
-  import Textfield from "@smui/textfield";
-  import HelperText from "@smui/textfield/helper-text";
   import { TiMail } from "svelte-icons-pack/ti";
   import Button from "$lib/components/ui/Button.svelte";
   import { validateEmail } from "$lib/utils/general";
@@ -9,20 +7,17 @@
   import HeaderIcon from "$lib/components/layout/header/HeaderIcon.svelte";
   import DropdownMenu from "$lib/components/ui/dropdown-menu/DropdownMenu.svelte";
   import DropdownMenuItem from "$lib/components/ui/dropdown-menu/DropdownMenuItem.svelte";
+  import TextInput from "$lib/components/ui/TextInput.svelte";
 
   let isOpen = $state(false);
   let dropdownTriggerElement: HTMLButtonElement | undefined = $state();
-  let isValidEmailInput = $state(true);
   let emailInput: string = $state("");
+  let isValidEmailInput = $derived(validateEmail(emailInput));
 
   function handleIconClick() {
     if (dropdownTriggerElement) {
       isOpen = !isOpen;
     }
-  }
-
-  function handleInput() {
-    isValidEmailInput = validateEmail(emailInput);
   }
 
   function subscribeToNewsletter() {
@@ -46,32 +41,33 @@
         <h2>Subscribe to our newsletter</h2>
       </DropdownMenuItem>
       <DropdownMenuItem classNames="w-full">
-        <Textfield
-          style="width: 100%"
-          input$style="color: white"
-          label$style="color: white"
-          value={emailInput}
-          type="email"
-          invalid={!isValidEmailInput && emailInput !== ""}
-          on:input={handleInput}
+        <div class="mt-2 w-full">
+          <TextInput
+            bind:value={emailInput}
+            type="email"
+            invalid={!isValidEmailInput && emailInput !== ""}
+          >
+            {#snippet label()}
+              <div class="flex">
+                <Icon
+                  className="pb-1 mr-0.5 flex content-center"
+                  color="white"
+                  src={TiMail}
+                  size="20"
+                />
+                Email
+              </div>
+            {/snippet}
+          </TextInput>
+        </div>
+        <p
+          class="pointer-events-none select-none {!isValidEmailInput &&
+          emailInput !== ''
+            ? 'opacity-100'
+            : 'opacity-0'} text-error"
         >
-          <svelte:fragment slot="label">
-            <div class="flex">
-              <Icon
-                className="pb-1 mr-0.5 flex content-center"
-                color="white"
-                src={TiMail}
-                size="20"
-              />
-              Email
-            </div>
-          </svelte:fragment>
-          <HelperText slot="helper" style="color: red; user-select: none">
-            {#if !isValidEmailInput && emailInput !== ""}
-              Please enter a valid email address.
-            {/if}
-          </HelperText>
-        </Textfield>
+          Please enter a valid email address.
+        </p>
       </DropdownMenuItem>
       <DropdownMenuItem classNames="w-full">
         <Button
---
+++ src/lib/components/ui/TextInput.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    value: string;
+    type: import("svelte/elements").HTMLInputTypeAttribute;
+    label?: Snippet;
+    helperText?: Snippet;
+    className?: string;
+    invalid?: boolean;
+  }
+
+  let {
+    value = $bindable(),
+    type,
+    label,
+    helperText,
+    className = "",
+    invalid = false
+  }: Props = $props();
+</script>
+
+<div class="pt-2.5">
+  <div class="relative">
+    <input
+      {type}
+      bind:value
+      class="peer h-10 w-full border-b-2 border-white bg-transparent text-white focus:border-primary focus:outline-none {invalid
+        ? 'focus:border-error'
+        : ''} {className}"
+    />
+    {#if label}
+      <label
+        class="pointer-events-none absolute left-0 select-none text-sm transition-all peer-focus:-top-3.5 peer-focus:text-sm {value
+          ? '-top-3.5'
+          : 'top-2 text-base'}"
+      >
+        {@render label()}
+      </label>
+    {/if}
+    {#if helperText}
+      {@render helperText()}
+    {/if}
+  </div>
+</div>
