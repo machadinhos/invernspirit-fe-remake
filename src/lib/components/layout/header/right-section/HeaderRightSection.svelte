9e8bef1368be848aab4c8dd027ccd911b2c2c17d
machadinhos <pbmachado2002@gmail.com>
2024-10-30 12:41:49 +0000

    add user dropdown menu

--- src/lib/components/layout/header/right-section/HeaderRightSection.svelte
+++ src/lib/components/layout/header/right-section/HeaderRightSection.svelte
@@ -1,7 +1,18 @@
-<script>
+<script lang="ts">
   import HeaderIcon from "$lib/components/layout/header/HeaderIcon.svelte";
   import { BiSolidCart } from "svelte-icons-pack/bi";
   import { FaSolidUser } from "svelte-icons-pack/fa";
+  import DropdownMenu from "$lib/components/ui/dropdown-menu/DropdownMenu.svelte";
+  import DropdownMenuItem from "$lib/components/ui/dropdown-menu/DropdownMenuItem.svelte";
+
+  let isOpen = $state(false);
+  let dropdownTriggerElement: HTMLButtonElement | undefined = $state();
+
+  function handleIconClick() {
+    if (dropdownTriggerElement) {
+      isOpen = !isOpen;
+    }
+  }
 </script>
 
 <div class="flex items-center justify-end gap-4">
@@ -9,9 +20,27 @@
     <option>pt</option>
     <option>es</option>
   </select>
-  <a href="/" class="h-fit w-fit">
-    <HeaderIcon src={FaSolidUser} />
-  </a>
+  <div>
+    <HeaderIcon
+      bind:ref={dropdownTriggerElement}
+      src={FaSolidUser}
+      onclick={handleIconClick}
+    />
+    <DropdownMenu
+      className="p-5"
+      bind:isOpen
+      triggerElement={dropdownTriggerElement}
+    >
+      <div class="flex flex-col gap-4">
+        <DropdownMenuItem>
+          <a class="hover:text-primary" href="/sign-in">Sign In</a>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <a class="hover:text-primary" href="/sign-up">Sign Up</a>
+        </DropdownMenuItem>
+      </div>
+    </DropdownMenu>
+  </div>
   <a href="/" class="h-fit w-fit">
     <HeaderIcon src={BiSolidCart} />
   </a>
