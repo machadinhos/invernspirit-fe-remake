44d52b5a67888bb7c8ac6950f53f92a67b574974
machadinhos <pbmachado2002@gmail.com>
2024-11-04 10:07:44 +0000

    make title be on top faq

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -180,40 +180,45 @@
   };
 </script>
 
-<div class="flex h-full">
-  <div class="flex h-full w-1/5 flex-col bg-background">
-    <div class="p-4">
-      <h2 class="text-lg font-semibold">Contents</h2>
+<div class="flex h-full flex-col">
+  <h1 class="py-6 text-center text-4xl font-bold">
+    Frequently Asked Questions
+  </h1>
+
+  <div class="flex h-[calc(100%-theme(spacing.24))] flex-1">
+    <div class="flex h-full w-1/5 flex-col bg-background">
+      <div class="p-4">
+        <h2 class="text-lg font-semibold">Contents</h2>
+      </div>
+      <div class="flex-1 overflow-y-auto">
+        {#each faqData as item, index}
+          <button
+            id="faq-toc-{index}"
+            class="w-full rounded-xl px-4 py-2 transition-all duration-300 {focusedIndex ===
+            index
+              ? 'bg-primary font-medium'
+              : 'hover:bg-secondary-background'}"
+            onclick={() => handleTocClick(index)}
+          >
+            {item.question}
+          </button>
+        {/each}
+      </div>
     </div>
-    <div class="flex-1 overflow-y-auto">
+
+    <div class="flex h-full flex-1 flex-col items-center overflow-y-auto pb-3">
       {#each faqData as item, index}
-        <button
-          id="faq-toc-{index}"
-          class="w-full rounded-xl px-4 py-2 transition-all duration-300 {focusedIndex ===
+        <div
+          id="faq-question-{index}"
+          class="w-[90%] transform rounded-lg p-6 transition-all duration-300 {focusedIndex ===
           index
-            ? 'bg-primary font-medium'
-            : 'hover:bg-secondary-background'}"
-          onclick={() => handleTocClick(index)}
+            ? 'scale-105 bg-background shadow-lg'
+            : 'opacity-50'}"
         >
-          {item.question}
-        </button>
+          <h2 class="mb-2 text-xl font-semibold">{item.question}</h2>
+          <p>{item.answer}</p>
+        </div>
       {/each}
     </div>
-  </div>
-
-  <div class="flex h-full flex-1 flex-col items-center overflow-y-auto pb-3">
-    <h1 class="mb-12 text-center text-4xl font-bold">FAQ</h1>
-    {#each faqData as item, index}
-      <div
-        id="faq-question-{index}"
-        class="w-[90%] transform rounded-lg p-6 transition-all duration-300 {focusedIndex ===
-        index
-          ? 'scale-105 bg-background shadow-lg'
-          : 'opacity-50'}"
-      >
-        <h2 class="mb-2 text-xl font-semibold">{item.question}</h2>
-        <p>{item.answer}</p>
-      </div>
-    {/each}
   </div>
 </div>
