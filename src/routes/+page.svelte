a65fb870e6ebd1568d23c9347fbb15da07f4fe26
machadinhos <pbmachado2002@gmail.com>
2024-11-07 12:21:50 +0000

    finish home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { imageLinks } from '$lib/constants/links';
   import GrainyFilter from '$lib/components/ui/GrainyFilter.svelte';
-  import { fade } from 'svelte/transition';
   import cart from '$lib/state/cart.svelte';
+  import Button from '$lib/components/ui/Button.svelte';
 
   let currentImageIndex = $state(0);
   let nextImageIndex = 1;
@@ -17,7 +17,6 @@
 </script>
 
 <div
-  transition:fade
   class="pointer-events-none fixed inset-0 -z-10 h-full w-full select-none overflow-hidden grayscale filter"
 >
   <GrainyFilter />
@@ -33,5 +32,25 @@
   {/each}
 </div>
 
-<button onclick={() => cart.addProduct()}>{'test'}</button>
-{cart.value.length}
+<div class="flex h-full w-full flex-col items-center justify-center">
+  <div>
+    <div class="mb-4 select-none">
+      <div>
+        <div class="flex gap-4 font-thin">
+          <h1 class="text-8xl">INVERN</h1>
+          <div class="flex flex-col justify-center">
+            <p>SPIRIT</p>
+            <p>OF THE WINTER</p>
+          </div>
+        </div>
+        <div class="h-0.5 w-full bg-white"></div>
+      </div>
+      <h2 class="text-3xl">EACH PIECE, UNIQUELY YOURS.</h2>
+    </div>
+    <Button className="px-3">
+      <a href="/"> Explore our collection </a>
+    </Button>
+  </div>
+  <button onclick={() => cart.addProduct()}>{'test'}</button>
+  {cart.value.length}
+</div>
