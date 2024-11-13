0527d8d4579509a71c79100363e6a8f98b5da44e
machadinhos <pbmachado2002@gmail.com>
2024-11-13 11:56:47 +0000

    create generic FlyUpElement to share fly up behavior and create DrawerItem element

---
+++ src/lib/components/ui/FlyUpElement.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { fly, type FlyParams } from 'svelte/transition';
+  import { quintInOut } from 'svelte/easing';
+
+  interface Props {
+    children: import('svelte').Snippet;
+    classNames?: string;
+    duration: FlyParams['duration'];
+  }
+
+  let { classNames, children, duration }: Props = $props();
+</script>
+
+<div class="overflow-hidden">
+  <div
+    class="w-fit {classNames}"
+    transition:fly={{
+      duration: duration,
+      y: window.innerHeight,
+      easing: quintInOut
+    }}
+  >
+    {@render children()}
+  </div>
+</div>
