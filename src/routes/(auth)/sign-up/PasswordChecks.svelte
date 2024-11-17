6f87230ec56ed64cb33e52abb61c00c288898d97
machadinhos <pbmachado2002@gmail.com>
2024-11-17 22:46:35 +0000

    simplify password validation

---
+++ src/routes/(auth)/sign-up/PasswordChecks.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { validatePassword } from '$lib/utils/input-validation';
+  import { Icon } from 'svelte-icons-pack';
+  import { FaSolidCheck, FaSolidX } from 'svelte-icons-pack/fa';
+
+  interface Props {
+    password: string;
+  }
+
+  let { password }: Props = $props();
+
+  let { errors } = $derived(validatePassword(password));
+</script>
+
+{#snippet passwordCheck(text: string, error: boolean)}
+  <div class="flex items-center gap-2">
+    <div class={error ? 'text-error' : 'text-success'}>
+      <Icon src={error ? FaSolidX : FaSolidCheck} />
+    </div>
+    {text}
+  </div>
+{/snippet}
+
+{@render passwordCheck('Between 10 and 128 characters.', errors.missingRequiredLengthError)}
+{@render passwordCheck('At least 1 uppercase letter.', errors.missingUppercaseLetterError)}
+{@render passwordCheck('At least 1 special character.', errors.missingSpecialCharactersError)}
