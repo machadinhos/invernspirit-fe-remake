e63125416425c28087f9c73b8a8fd22e668f51f3
machadinhos <pbmachado2002@gmail.com>
2024-11-17 22:47:28 +0000

    add eslint-plugin-security

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import perfectionist from 'eslint-plugin-perfectionist';
+import security from 'eslint-plugin-security';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
@@ -9,6 +10,7 @@ export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs['flat/recommended'],
+  security.configs.recommended,
   prettier,
   ...svelte.configs['flat/prettier'],
   {
