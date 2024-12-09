8ffb3d7f6cc45fcef603ab4eea9a278ea536a46b
machadinhos <pbmachado2002@gmail.com>
2024-12-09 01:53:50 +0000

    collections page (work in progress)

---
+++ src/routes/shop/collections/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<div class="flex h-full justify-center">
+  {#each data.collections as collection}
+    <a href={`/shop/collections/${collection.collectionId}`}>
+      <div class="h-full w-[200px]">
+        <img class="h-full object-cover object-center" alt={collection.image.alt} src={collection.image.url} />
+      </div>
+    </a>
+  {/each}
+</div>
