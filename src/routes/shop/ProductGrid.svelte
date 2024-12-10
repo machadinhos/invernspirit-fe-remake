704bc079589e85dc0a53ea453817cb04eee52be3
machadinhos <pbmachado2002@gmail.com>
2024-12-10 20:04:02 +0000

    finish collections and collection by id pages

---
+++ src/routes/shop/ProductGrid.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type {Product} from "$types";
+  import ProductCard from "./ProductCard.svelte";
+
+  interface Props {
+    products: Product[]
+  }
+
+  let { products }: Props = $props();
+</script>
+
+<div class="grid grid-cols-5">
+  {#each products as { productId, productName, priceInCents, images }}
+    <ProductCard
+      id={productId}
+      name={productName}
+      imageAlt={images[0].alt}
+      imageSrc={images[0].url}
+      price={priceInCents / 100}
+      stock={5}
+    />
+  {/each}
+</div>
--- src/routes/shop/collections/[id]/+page.svelte
+++ src/routes/shop/collections/[id]/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+  import { BsArrowReturnLeft } from 'svelte-icons-pack/bs';
+  import { Icon } from 'svelte-icons-pack';
+  import type { PageData } from './$types';
+  import ProductGrid from "../../ProductGrid.svelte";
 
-  const { id } = $page.params;
+  let { data }: { data: PageData } = $props();
 </script>
 
-<p>{id}</p>
+<div class="flex h-full items-center">
+  <div class="w-1/2 pl-40">
+    <h1 class="text-6xl">{data.collection.collectionName}</h1>
+    <div class="mt-5 w-fit">
+      <a href="/shop/collections">
+        <Icon size="40" src={BsArrowReturnLeft} />
+      </a>
+    </div>
+  </div>
+  <div class="w-1/2">
+    <p>{data.collection.description}</p>
+  </div>
+</div>
+
+<ProductGrid products={data.collection.products} />
