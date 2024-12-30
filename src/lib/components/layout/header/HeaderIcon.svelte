4f6828947bb3f33885dbe9ddd3bde59a9cc235b3
machadinhos <pbmachado2002@gmail.com>
2024-12-30 22:53:42 +0000

    add shrink animation to buttons and icons on click

--- src/lib/components/layout/header/HeaderIcon.svelte
+++ src/lib/components/layout/header/HeaderIcon.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Icon, type IconType } from 'svelte-icons-pack';
   import type { HTMLButtonAttributes } from 'svelte/elements';
+  import { ShrinkOnClickWrapper } from '$components';
 
   interface Props {
     src: IconType;
@@ -27,21 +28,23 @@
   </div>
 {/snippet}
 
-{#if type === 'button'}
-  <button
-    bind:this={ref}
-    class="outer-element relative flex h-9 w-9 cursor-pointer items-center justify-center"
-    aria-label={ariaLabel}
-    {onclick}
-    type="button"
-  >
-    {@render innerElements()}
-  </button>
-{:else}
-  <div class="outer-element relative flex h-9 w-9 cursor-pointer items-center justify-center">
-    {@render innerElements()}
-  </div>
-{/if}
+<ShrinkOnClickWrapper>
+  {#if type === 'button'}
+    <button
+      bind:this={ref}
+      class="outer-element relative flex h-9 w-9 cursor-pointer items-center justify-center"
+      aria-label={ariaLabel}
+      {onclick}
+      type="button"
+    >
+      {@render innerElements()}
+    </button>
+  {:else}
+    <div class="outer-element relative flex h-9 w-9 cursor-pointer items-center justify-center">
+      {@render innerElements()}
+    </div>
+  {/if}
+</ShrinkOnClickWrapper>
 
 <style>
   .outer-element > div {
---
+++ src/lib/components/ui/ShrinkOnClickWrapper.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  interface Props {
+    children: import('svelte').Snippet;
+  }
+
+  let { children }: Props = $props();
+</script>
+
+<div class="transition-all active:scale-90">
+  {@render children()}
+</div>
