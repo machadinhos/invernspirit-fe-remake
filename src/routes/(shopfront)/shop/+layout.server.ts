c1ea72244915659ea3c211203f7c8a3f2fca0e47
machadinhos <pbmachado2002@gmail.com>
2024-12-31 15:04:53 +0000

    solve build making multiple times the same request to the backend

--- src/routes/(shopfront)/shop/+layout.server.ts
+++ src/routes/(shopfront)/shop/+layout.server.ts
@@ -1,6 +1,16 @@
 import { beClient } from '$service-server';
+import { building } from '$app/environment';
+import type { Collection } from '$types';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = () => {
+let cachedData: { collections: Collection[] };
+
+export const load: LayoutServerLoad = async () => {
+  if (building) {
+    if (!cachedData) {
+      cachedData = await beClient.getAllCollections();
+    }
+    return cachedData;
+  }
   return beClient.getAllCollections();
 };
