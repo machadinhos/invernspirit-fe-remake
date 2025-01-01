0582e9ee87e72eece6661abb768b29d40ba74020
machadinhos <pbmachado2002@gmail.com>
2025-01-01 13:14:17 +0000

    add loading cart item animation

---
+++ src/routes/(shopfront)/cart/LoadingCartItem.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { LoadingBox } from '$components';
+</script>
+
+<div class="flex items-center justify-between">
+  <div class="flex gap-4">
+    <LoadingBox className="h-[100px] w-[100px]" />
+    <div>
+      <div class="flex h-9 items-end">
+        <LoadingBox className="h-[90%] w-36" />
+      </div>
+      <div class="flex h-7 items-end">
+        <LoadingBox className="h-[90%] w-20" />
+      </div>
+      <div class="flex h-7 items-end">
+        <LoadingBox className="h-[90%] w-32" />
+      </div>
+    </div>
+  </div>
+  <LoadingBox className="h-5 w-5" />
+</div>
