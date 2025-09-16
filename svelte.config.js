b1d920b0f2c471b21ad507b5aad2ec0be7446e69
machadinhos <pbmachado2002@gmail.com>
2025-09-16 19:11:53 +0100

    feat: enable svelte inspector vite plugin option

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
 
+  vitePlugin: {
+    inspector: true,
+  },
+
   kit: {
     adapter: adapter({ fallback: 'plaintext' }),
 
